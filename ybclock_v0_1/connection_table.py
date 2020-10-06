5c0f87ee1903c3328038219a8d0b8a6452e3792d
Ybclock Operator <vvybclock@gmail.com>
2020-10-06 12:51:57 -0400

    Reorder Device instantiation to reference objects created before they are.

--- ybclock_v0_1/connection_table.py
+++ ybclock_v0_1/connection_table.py
@@ -10,15 +10,34 @@ def define_connection_table():
 		usbport           	= 'COM5'
 	)
 	
+
+	### NI Cards
+	NI_PCI_6284( #digital card
+		name            	= 'ni_pci_6284_dev6',
+		parent_device   	= digital_clock.clockline,
+		clock_terminal  	= '',
+		MAX_name        	= 'Dev6',
+		acquisition_rate	= 1e3
+	)
+	
+
+	### Secondary Pseudoclock
 	PineBlaster(
 		name              	= 'analog_clock',
-		trigger_device    	= None,
+		trigger_device    	= ni_pci_6284_dev6,
 		trigger_connection	= None,
 		usbport           	= 'COM7'
 	)
 	
 
-	### NI Cards
+	### Secondary NI Cards
+	NI_PCI_6713( #analog out
+		name          	= 'ni_pci_6713_dev5',
+		parent_device 	= analog_clock.clockline,
+		clock_terminal	= '',
+		MAX_name      	='Dev5'
+	)
+
 	NI_PCI_6723( #analog out
 		name          	= 'ni_pci_6723_dev3',
 		parent_device 	= analog_clock.clockline,
@@ -32,22 +51,6 @@ def define_connection_table():
 		clock_terminal	= '',
 		MAX_name      	='Dev4'
 	)
-
-	NI_PCI_6713( #analog out
-		name          	= 'ni_pci_6713_dev5',
-		parent_device 	= analog_clock.clockline,
-		clock_terminal	= '',
-		MAX_name      	='Dev5'
-	)
-
-	NI_PCI_6284( #digital card
-		name            	= 'ni_pci_6284_dev6',
-		parent_device   	= digital_clock.clockline,
-		clock_terminal  	= '',
-		MAX_name        	= 'Dev6',
-		acquisition_rate	=1e3
-	)
-
 
 	#====================
 	# Dev 3 Connections
