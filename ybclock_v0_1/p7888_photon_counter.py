8fbc2cb2f466c09bffd52be5fbd64c4d3f539a34
Ybclock Operator <vvybclock@gmail.com>
2020-10-09 13:01:12 -0400

    Load dp7888.dll. Mentioned in Section 5.4 of the photon counting card docs.

--- ybclock_v0_1/p7888_photon_counter.py
+++ ybclock_v0_1/p7888_photon_counter.py
@@ -1,8 +1,21 @@
-### Test 1: Load the COMCTL.DLL, this is the DLL specified in figure 5.9 of the P7888 manual.
-###		It appears that its actually "comctl32.dll" as a window search revealed.
+# Test 1: Load the COMCTL.DLL, this is the DLL specified in figure 5.9 of the
+# P7888 manual. It appears that its actually "comctl32.dll" as a window search
+# revealed.
 import ctypes
 
-#comtec_dll = ctypes.cdll.LoadLibrary("COMCTL.DLL")
-comtec_dll = ctypes.cdll.LoadLibrary("comctl32.dll")
+# Here we load the Dynamic Linked Library that allows sending commands to the
+# P7888 card provided we have enabled remote mode in the P7888 server. See
+# section 5.2 (p. 5-9) in the p7888 docs.
+comtec_dll = ctypes.windll.LoadLibrary("comctl32.dll")
+
+# In section 5.4, they instead reference "dp7888.dll"! This is what is used
+# for controlling the server.
+
+dp7888_dll = ctypes.windll.LoadLibrary("dp7888.dll")
 
 print(comtec_dll)
+print(dp7888_dll)
+
+# Test 2: Print structures defined in the DLL.
+
+print(dp7888_dll.Continue)
