ef3799569840fc7540bdb8d35842a1168201d637
Enrique Mendez <eqm@mit.edu>
2020-10-26 14:55:16 -0400

    Add docstring explaining hardware versioning.

---
+++ ybclock_v0_1/__init__.py
@@ -0,0 +1,18 @@
+"""
+	ybclock_v0_1 -- Where we store our connection tables and sequences for our
+	current version of hardware. 
+
+	Versioning -- The idea for the version number is to separate
+	mutually exclusive hardware wirings. For example when we use something that
+	was driving the mot now drives an AOM, we should change version number so as
+	not to run (old AOM) code that could break our MOT by over-driving it.
+
+	Documentation as to what changed shall go into HW_VERSIONING.md
+
+	If however we are installing new channels without disconnecting old channels
+	, then the hardware version shall remain the same, and the git shall account
+	for improvements and new connections.
+
+	TODO -- Write a program for moving code from hardware version to hardware 
+	version.
+"""
