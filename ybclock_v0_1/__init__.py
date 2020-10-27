0ee2b2abe7765dfcacb0a71b36361a4728f0f5ec
Enrique Mendez <eqm@mit.edu>
2020-10-27 11:54:29 -0400

    Add .md syntax.

--- ybclock_v0_1/__init__.py
+++ ybclock_v0_1/__init__.py
@@ -1,11 +1,15 @@
 """
-	ybclock_v0_1 -- Where we store our connection tables and sequences for our
+	#ybclock_v0_1
+	
+	Where we store our connection tables and sequences for our
 	current version of hardware. 
 
-	Versioning -- The idea for the version number is to separate
-	mutually exclusive hardware wirings. For example when we use something that
-	was driving the mot now drives an AOM, we should change version number so as
-	not to run (old AOM) code that could break our MOT by over-driving it.
+	## Versioning
+
+	The idea for the version number is to separate mutually exclusive hardware
+	wirings. For example when we use something that was driving the mot now drives
+	an AOM, we should change version number so as not to run (old AOM) code that
+	could break our MOT by over-driving it.
 
 	Documentation as to what changed shall go into HW_VERSIONING.md
 
@@ -13,6 +17,8 @@
 	, then the hardware version shall remain the same, and the git shall account
 	for improvements and new connections.
 
-	TODO -- Write a program for moving code from hardware version to hardware 
+	#To Do
+
+	* Write a program for moving code from hardware version to hardware 
 	version.
 """
