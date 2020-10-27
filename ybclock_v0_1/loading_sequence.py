bbd6773d04c53b693ea39a9da811572c52ec1216
Ybclock Operator <vvybclock@gmail.com>
2020-10-27 09:58:44 -0400

    Remove global variable reference so pdoc can compile.

--- ybclock_v0_1/loading_sequence.py
+++ ybclock_v0_1/loading_sequence.py
@@ -2,12 +2,6 @@ from labscript import start, stop, AnalogOut, DigitalOut
 from labscriptlib.ybclock_v0_1.connection_table import define_connection_table
 from labscriptlib.ybclock_v0_1.subsequences import *
 
-blue_mot_duration 
-
-
-
-
-
 
 if __name__ == '__main__':
 
