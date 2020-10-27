866026f62762be15e1fafc8548791d40d42e9ff9
Enrique Mendez <eqm@mit.edu>
2020-10-27 12:08:16 -0400

    Add .md syntax.

--- ybclock_v0_1/subsequences/dummy_filename.py
+++ ybclock_v0_1/subsequences/dummy_filename.py
@@ -1,6 +1,11 @@
 '''
-	dummy_filename -- blank file for testing imports into __init__.py/folder 
+	#dummy_filename 
+
+	blank file for testing imports into __init__.py/folder 
 	based modules.
+
+	It also serves an example for how two import structures can exist depending
+	on code in __init__.py
 '''
 
 def dummy_subsequence():
