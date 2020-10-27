18fcdc927f01c95574d599e7e5593051ebb82b6c
Enrique Mendez <eqm@mit.edu>
2020-10-27 11:54:48 -0400

    Add docstrings.

--- ybclock_v0_1/connection_table.py
+++ ybclock_v0_1/connection_table.py
@@ -1,10 +1,34 @@
+'''
+	#connection_table.py
+
+	Labscript requires this file with this name so that it can compile what it
+	needs for BLACS.
+'''
+
 from labscript import start, stop
 
 from labscriptlib.ybclock_v0_1.connection_functions import define_hardware_cards
 from labscriptlib.ybclock_v0_1.connection_functions import define_channels
 
 def define_connection_table():
+	''' Here we break out the connection table into smaller function calls,
+	which are in turn broken up and held in connection_functions module.
+	This allows for conceptual partitioning and easier navigation of the code.
 
+	More crucially, it's necessary for allowing importation into other sequences.
+
+	I faced problems without defining this function. `define_connection_table`
+	needs to be called in the `example_sequence.py` runmanager is compiling.
+
+	The problem is when compiling multiple shots, each time you do  'from
+	connection_table import *` it won't run each of the device/channel declaration
+	functions again, and so you would need to reset each compiler instance if you
+	wanted to run another shot.
+
+	With the function call format, it allows you to run the device/channel
+	declaration everytime you compile, as desired.
+	'''
+	
 	define_hardware_cards()
 	
 	define_channels()
