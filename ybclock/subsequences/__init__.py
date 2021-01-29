69b45fe6d043345f6f8c737ed65c918d5c09ea09
Ybclock Operator <vvybclock@gmail.com>
2021-01-29 12:27:39 -0500

    Add docs.

--- ybclock/subsequences/__init__.py
+++ ybclock/subsequences/__init__.py
@@ -1,3 +1,31 @@
+'''
+This module holds the building blocks of a full experimental sequence, for 
+easy re-use.
+
+Please break up functions into smaller files, if you don't want long import strings like
+`import labscriptlib.ybclock.subsequences.chis_sequences.load_mot`
+add a `from ... import *` in the `__init__.py`. This will load it in the parent module,
+i.e., only a `from labscriptlib.ybclock.subsequences import load_mot` is necessary.
+
+Even more compact is 
+```
+import labscriptlib.ybclock.subsequences as subseq
+
+subseq.load_mot()
+```
+
+or
+```
+from labscriptlib.ybclock.subsequences import *
+
+load_mot()
+```
+
+This let's us maintain small python scripts without loss of
+simplicity with importing.
+
+
+'''
 print("Importing Subsequences...")
 from .dummy_filename import *
 from .loading_subsequences import *
