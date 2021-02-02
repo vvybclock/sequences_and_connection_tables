a3edcdc9e061328be27fb4d75703155b8755123d
Ybclock Operator <vvybclock@gmail.com>
2021-02-01 23:58:56 -0500

    Add test sequence.

---
+++ ybclock/test_sequences/test_cameras.py
@@ -0,0 +1,11 @@
+from labscript import start, stop
+from labscriptlib.ybclock.connection_table import define_connection_table
+if __name__ == '__main__':
+	define_connection_table()
+	# Begin issuing labscript primitives
+	# start() elicits the commencement of the shot
+	start()
+	for x in range(1,4):
+		wide_angle_cam.expose(t=x*0.1,	name = f'wide_test_pic_{x}',	trigger_duration=0.03)
+		isometric_cam.expose(t=x*0.1, 	name=f'iso_test_pic_{x}',   	trigger_duration=0.03)
+	stop(1)
