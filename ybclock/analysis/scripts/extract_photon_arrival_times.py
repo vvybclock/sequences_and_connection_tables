b063b85b7786f523aed552a62c2c488b0fe1ba64
Enrique Mendez <eqm@mit.edu>
2021-03-03 12:29:31 -0500

    Test data extraction.

--- ybclock/analysis/scripts/extract_photon_arrival_times.py
+++ ybclock/analysis/scripts/extract_photon_arrival_times.py
@@ -20,13 +20,15 @@ if __name__ == '__main__':
 	#load data from the last run
 	path #path is defined in `from lyse ...`
 	with h5py.File(path,'a') as hdf:
+		print(hdf['/data/photon_arrivals'])
+
 		#create folder for photon counts
-		grp = hdf.create_group("/data/photon_arrivals")
-		file_array = np.fromfile(p7888.p7888_data_file,dtype='<i4')
-		lst_file = grp.create_dataset("all_arrivals",data=file_array)
-		lst_file.attrs.create("Description", data=
-			'Contains the very large photon arrival file. This file contains the data of arrival times from multiple shots. Not just the one shot we care about here.'
-		)
+		# grp = hdf.create_group("/data/photon_arrivals")
+		# file_array = np.fromfile(p7888.p7888_data_file,dtype='<i4')
+		# lst_file = grp.create_dataset("all_arrivals",data=file_array)
+		# lst_file.attrs.create("Description", data=
+		#	'Contains the very large photon arrival file. This file contains the data of arrival times from multiple shots. Not just the one shot we care about here.'
+		# )
 
 	#extract the .lst binary
 
