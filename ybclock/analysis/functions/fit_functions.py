948cdb53e84856da240e56affec1b3e908d0c131
Ybclock Operator <vvybclock@gmail.com>
2021-03-03 20:12:18 -0500

    Working fit.

---
+++ ybclock/analysis/functions/fit_functions.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.optimize import leastsq
+
+def lorentzian(x, x0, a, gamma, offset):
+	'''
+	x    	= position,
+	x0   	= peak center,
+	a    	= amplitude of peak,
+	gamma	= Half width half max
+	'''
+	return a*gamma**2/(gamma**2 + (x-x0)**2) + offset
+
+def residuals_of_lorentzian(params, x_data, y_data):
+	''' Calculates the residuals of the fit.'''
+	diff = [lorentzian(x, params[0], params[1], params[2], params[3]) - y for x,y in zip(x_data,y_data)]
+	return diff
+
+def fit_single_cavity_peak(data,start,end,bin_interval):
+	''' fits a single_cavity_peak. assumes unbinned photon arrival times for data'''
+
+	#bin the data
+	(hist, bin_edges) = np.histogram(
+		data,
+		bins=np.arange(start,end, bin_interval)
+	)
+
+	#estimate initial parameters
+	amplitude = np.amax(hist)
+	x0_index = np.argmax(hist)
+	x0 = bin_edges[x0_index]
+	locations_greater_than_half_max = np.where(hist > amplitude/2)[0]
+	right_time = bin_edges[locations_greater_than_half_max[-1]]
+	left_time = bin_edges[locations_greater_than_half_max[0]]
+	gamma = (right_time-left_time + bin_interval)/2  #in case the peak is one bin wide.
+	offset = 0
+
+	#format the parameters
+	init_guess = [x0, amplitude, gamma, offset]
+
+	(best_guess, cov_best_guess) = leastsq(residuals_of_lorentzian, init_guess, args=(bin_edges[:-1]+bin_interval/2,hist))
+
+
+	return best_guess, cov_best_guess
+
--- ybclock/analysis/scripts/plot_photon_arrival_times.py
+++ ybclock/analysis/scripts/plot_photon_arrival_times.py
@@ -2,22 +2,38 @@ from lyse import *
 import numpy as np
 import matplotlib.pyplot as plt
 from labscriptlib.ybclock.analysis.functions.metadata import extract_sequence_repetition_numbers, extract_date,extract_sequence_name
-
+import labscriptlib.ybclock.analysis.functions.fit_functions as fit_functions
 run = Run(path)
 
+#extract metadata
 (sequence_number, repetition_number)	= extract_sequence_repetition_numbers(path)
 date                                	= extract_date(path)
 sequence_name                       	= extract_sequence_name(path)
 
+#extract data
 photon_arrival_times = run.get_result(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
 
+#plot data
 plt.hist(
 	photon_arrival_times,
-	bins=np.arange(0,30e-3, 200e-6)
+	bins=np.arange(0,30e-3, 200e-6),
+	align='mid'
 )
 
 plt.title(f"({date}) #{sequence_number}_r{repetition_number}\n{sequence_name}")
 
 plt.ylabel("Photon Counts, (200us Bin)")
 plt.xlabel("Time (s)")
-print(photon_arrival_times)
+
+#perform fit
+(best_guess, cov_best_guess) = fit_functions.fit_single_cavity_peak(
+	data=photon_arrival_times,
+	start=0,
+	end=30e-3,
+	bin_interval=200e-6,
+)
+
+x = np.arange(0,30e-3,20e-6)
+y = fit_functions.lorentzian(x=x,x0=best_guess[0], a=best_guess[1], gamma=best_guess[2], offset=best_guess[3])
+
+plt.plot(x,y)
