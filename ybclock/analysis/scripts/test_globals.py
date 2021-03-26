5d791cacec25b16117395995f7bfe591b9f4c135
Simone <colombos@mit.edu>
2021-03-26 17:43:08 -0400

    Remove error

--- ybclock/analysis/scripts/test_globals.py
+++ ybclock/analysis/scripts/test_globals.py
@@ -8,7 +8,6 @@ Here is the code to call the globals in the analysis.
 '''
 run = Run(path)
 data_globals = run.get_globals()
-print(data_globals)
+print(data_globals['VCO_polynomial_interp'])
 
-print(data_sequence)
 # run.get_trace(probe_sideband_frequency)
