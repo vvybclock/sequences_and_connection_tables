f8e06aa6675aeafae77e3e792c3a8f4dec50dfa2
Ybclock Operator <vvybclock@gmail.com>
2021-03-31 15:01:27 -0400

    Update class to work in lyse easier. Add counter/method for keeping track of photon pulses.

--- ybclock/subsequences/exp_cavity.py
+++ ybclock/subsequences/exp_cavity.py
@@ -9,18 +9,25 @@ import numpy as np
 
 class ExperimentalCavity:
 	'''
-	Records parameters and saves them once the program quits.
+	Records parameters and saves them after each scan call.
 	'''
 	scan_parameters = {}
+	number_of_p7888_start_triggers = None
 
 	def __init__(self):
 		''' Try to create the metadata group if it doesn't exist. 
 		Clear the scan_parameters dict, as it seems to stay full after each shot compilation.'''
-
 		self.scan_parameters = {}
-		with h5py.File(compiler.hdf5_filename, 'a') as hdf5_file:
-			hdf5_file.require_group('metadata')
-			hdf5_file.require_group('/metadata/exp_cavity')
+		self.number_of_p7888_start_triggers = 0
+
+		try:
+			#only do this if we're compiling the shot.
+			with h5py.File(compiler.hdf5_filename, 'a') as hdf5_file:
+				print("Requring Metadata Folder.")
+				hdf5_file.require_group('metadata')
+				hdf5_file.require_group('/metadata/exp_cavity')
+		except:
+			pass
 
 	def save_parameters(self):
 		''' Pickle Parameters then Save to HDF'''
@@ -30,13 +37,34 @@ class ExperimentalCavity:
 			grp.attrs["scan_parameters"] = np.void(pickled_dict)
 
 
-	def get_parameters(self):
+	def get_parameters(self,path=None):
 		''' Load from HDF file then Unpickle '''
-		with h5py.File(compiler.hdf5_filename, 'a') as hdf5_file:
+		if compiler.hdf5_filename != None:
+			hdf5_filename = compiler.hdf5_filename
+		else:
+			#we'll assume it's in being used in lyse.
+			hdf5_filename = path
+
+		with h5py.File(hdf5_filename, 'r') as hdf5_file:
 			grp = hdf5_file['/metadata/exp_cavity']
 			void_pickled_dict = grp.attrs["scan_parameters"]
 			pickled_dict = void_pickled_dict.tobytes()
 			self.scan_parameters = pickle.loads(pickled_dict)
+		return self.scan_parameters
+
+	def pulse_p7888_start_trigger(self, t):
+		'''
+			This function sends a TTL pulse of 1ms duration to the photon counter.
+			It also keeps track of how many pulses we've sent for easier analysis.
+		'''
+		us = 1e-6
+		ms = 1e-3
+
+		p7888_start_trigger.enable(t)
+		p7888_start_trigger.disable(t + 500*us)
+
+		self.number_of_p7888_start_triggers += 1
+		return 1*ms
 
 	def scan(self,t, label, verbose=False):
 		'''
@@ -63,13 +91,15 @@ class ExperimentalCavity:
 		final_f   	= empty_cavity_frequency_sweep_initial+empty_cavity_frequency_sweep_range
 		samplerate	= empty_cavity_samples/(empty_cavity_sweep_duration*ms)
 
+
 		#record the parameters in a dictionary inside a list that holds dictionarys.
 		self.scan_parameters[label].append({
-			"t"         	: t,
-			"duration"  	: duration,
-			"initial_f" 	: initial_f,
-			"final_f"   	: final_f,
-			"samplerate"	: samplerate
+			"t"                    	: t,
+			"duration"             	: duration,
+			"initial_f"            	: initial_f,
+			"final_f"              	: final_f,
+			"samplerate"           	: samplerate,
+			"initial_start_trigger"	: self.number_of_p7888_start_triggers
 		})
 
 		#initial laser light management
@@ -97,9 +127,8 @@ class ExperimentalCavity:
 
 		#run the photon counter
 		while tloop < t:
-			p7888_start_trigger.enable(tloop)
-			p7888_start_trigger.disable(tloop + 500*us)
-			tloop += 1*ms
+			tloop += self.pulse_p7888_start_trigger(tloop)
+			
 
 		#turn off lights
 		probe_sideband_power_switch.disable(t)
