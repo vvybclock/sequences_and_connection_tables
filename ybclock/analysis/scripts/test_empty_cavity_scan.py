98e34fde7478f34f711f41bc34581d057002ceeb
Ybclock Operator <vvybclock@gmail.com>
2021-04-07 14:42:07 -0400

    Update get method to get_result_array

--- ybclock/analysis/scripts/test_empty_cavity_scan.py
+++ ybclock/analysis/scripts/test_empty_cavity_scan.py
@@ -15,79 +15,33 @@ from labscriptlib.ybclock.subsequences import ExperimentalCavity
 #analysis libs
 import numpy as np
 import matplotlib.pyplot as plt
-from labscriptlib.ybclock.analysis.functions.metadata import extract_sequence_repetition_numbers, extract_date,extract_sequence_name
 import labscriptlib.ybclock.analysis.functions.fit_functions as fit_functions
 from labscriptlib.ybclock.analysis.functions.empty_cavity_helper import empty_cavity_analysis
 
 run = Run(path)
 
-#extract metadata
-(sequence_number, repetition_number)	= extract_sequence_repetition_numbers(path)
-date                                	= extract_date(path)
-sequence_name                       	= extract_sequence_name(path)
+
 
 #extract cavity_scan_parameters metadata
 exp_cavity = ExperimentalCavity()
 cavity_scan_parameters = exp_cavity.get_parameters(path)
 
-print(f"Cavity Scan Parameters:\n\n\n{cavity_scan_parameters}\n\n\n\n")
+# print(f"Cavity Scan Parameters:\n\n\n{cavity_scan_parameters}\n\n\n\n")
 
 #extract globals
 # data_globals = run.get_globals()
 
 #extract data
-photon_arrival_times = run.get_result(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
+photon_arrival_times = run.get_result_array(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
 
 #check to see if we need to run empty cavity analysis
 for each_key in cavity_scan_parameters.keys():
 	if each_key == 'empty_cavity':
 		empty_cavity_analysis(
 			data=photon_arrival_times,
-			scan_parameters=cavity_scan_parameters[each_key]
+			scan_parameters=cavity_scan_parameters[each_key],
+			path=path
 		)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#plot data
-plt.hist(
-	photon_arrival_times,
-	bins=np.arange(0,30e-3, 200e-6),
-	align='mid'
- )
-
-#decorate plot
-plt.title(f"({date}) #{sequence_number}_r{repetition_number}\n{sequence_name}")
-plt.ylabel("Photon Counts, (200us Bin)")
-plt.xlabel("Time (s)")
-
 
 #*********************
 #	Fit Cavity Scans with MLE
