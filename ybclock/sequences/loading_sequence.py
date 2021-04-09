4cbdb6ac7becdd6fded9c515cea018402ff18b90
Ybclock Operator <vvybclock@gmail.com>
2021-04-09 18:52:58 -0400

    Update to also perform vacuum rabi scan.

--- ybclock/sequences/loading_sequence.py
+++ ybclock/sequences/loading_sequence.py
@@ -1,4 +1,8 @@
-from labscript import start, stop, AnalogOut, DigitalOut
+'''
+	Loads atoms from the Oven Up to the Optical Lattice, 
+	applies the large bias field, and measures the "Vacuum" Rabi Splitting.
+'''
+from labscript import start, stop, AnalogOut, DigitalOut,add_time_marker
 from labscriptlib.ybclock.connection_table import define_connection_table
 from labscriptlib.ybclock.subsequences import *
 
@@ -6,7 +10,8 @@ from labscriptlib.ybclock.subsequences import *
 if __name__ == '__main__':
 
 	define_connection_table()
-	
+	exp_cavity = ExperimentalCavity()
+
 	# Begin issuing labscript primitives
 	# start() elicits the commencement of the shot
 	start()
@@ -19,9 +24,19 @@ if __name__ == '__main__':
 
 	t += load_from_oven_to_optical_lattice(t)
 
-	# Stop the experiment shot with stop()
+	#ramp magnetic fields
+	add_time_marker(t, "Ramp Bias Fields.")
+	ramp_duration = 2*ms
+	z_bias_field.ramp(t, duration=ramp_duration, initial=-0.8,final=7.35,samplerate=1*kHz)
+	y_bias_field.ramp(t, duration=ramp_duration, initial=0.32,final=-1.86,samplerate=1*kHz)
+	x_bias_field.ramp(t, duration=ramp_duration, initial=5.29,final=-0.02,samplerate=1*kHz)
+
+	t += 200*ms
+
+	t += exp_cavity.scan(t, label='empty_cavity')
 
 	set_default_values(t)
+	# Stop the experiment shot with stop()
 	stop(t+1)
 
 print("Compiled loading_sequence!")
