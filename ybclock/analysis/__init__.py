8150bfa70633f084d68dbdad995ce7a6928f982d
Ybclock Operator <vvybclock@gmail.com>
2021-04-12 12:09:33 -0400

    Update docs.

--- ybclock/analysis/__init__.py
+++ ybclock/analysis/__init__.py
@@ -1,3 +1,7 @@
 '''
 This module stores our labscript based analysis functions and scripts.
+
+Scripts are what run in lyse, functions holds helper functions for simplifying these scripts.
+
+.. include:: ./README.md
 '''
