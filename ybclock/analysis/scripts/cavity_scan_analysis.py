47d516496106f3be648ee7238c412f02a41c6058
Ybclock Operator <vvybclock@gmail.com>
2021-04-13 09:08:58 -0400

    Wrap in if __name__ == "__main__" blocks for pdoc.

--- ybclock/analysis/scripts/cavity_scan_analysis.py
+++ ybclock/analysis/scripts/cavity_scan_analysis.py
@@ -22,38 +22,40 @@ from labscriptlib.ybclock.analysis.functions.empty_cavity_helper import empty_ca
 from labscriptlib.ybclock.analysis.functions.atoms_in_cavity_helper import atom_cavity_analysis
 import pickle
 
-#get data
-run = Run(path)
+
+if __name__ == '__main__':
+	#get data
+	run = Run(path)
 
 
-#extract cavity_scan_parameters metadata
-try:
-	exp_cavity = ExperimentalCavity()
-	cavity_scan_parameters = exp_cavity.get_parameters(path)
-except:
-	print("Error: Could not extract cavity_scan_parameters.")
+	#extract cavity_scan_parameters metadata
+	try:
+		exp_cavity = ExperimentalCavity()
+		cavity_scan_parameters = exp_cavity.get_parameters(path)
+	except:
+		print("Error: Could not extract cavity_scan_parameters.")
 
 
-#extract photon data
-try:
-	photon_arrival_times = run.get_result_array(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
-except:
-	print("Error: Could not extract photon_arrival_times.")
+	#extract photon data
+	try:
+		photon_arrival_times = run.get_result_array(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
+	except:
+		print("Error: Could not extract photon_arrival_times.")
 
 
 
-#check to see if we need to run any sort of cavity analysis
-for each_key in cavity_scan_parameters.keys():
-	if each_key == 'empty_cavity':
-		empty_cavity_analysis(
-			data=photon_arrival_times,
-			scan_parameters=cavity_scan_parameters[each_key],
-			path=path
-		)
-	if each_key == 'atoms_in_cavity':
-		print("Performing atom_cavity_analysis.")
-		atom_cavity_analysis(
-			data=photon_arrival_times,
-			scan_parameters=cavity_scan_parameters[each_key],
-			path=path
-		)
+	#check to see if we need to run any sort of cavity analysis
+	for each_key in cavity_scan_parameters.keys():
+		if each_key == 'empty_cavity':
+			empty_cavity_analysis(
+				data=photon_arrival_times,
+				scan_parameters=cavity_scan_parameters[each_key],
+				path=path
+			)
+		if each_key == 'atoms_in_cavity':
+			print("Performing atom_cavity_analysis.")
+			atom_cavity_analysis(
+				data=photon_arrival_times,
+				scan_parameters=cavity_scan_parameters[each_key],
+				path=path
+			)
--- ybclock/analysis/scripts/plot_photon_arrival_times.py
+++ ybclock/analysis/scripts/plot_photon_arrival_times.py
@@ -12,28 +12,29 @@ from labscriptlib.ybclock.analysis.functions.metadata import extract_sequence_re
 import labscriptlib.ybclock.analysis.functions.fit_functions as fit_functions
 
 
+if __name__ == '__main__':
 
-run = Run(path)
+	run = Run(path)
 
-#extract metadata
-(sequence_number, repetition_number)	= extract_sequence_repetition_numbers(path)
-date                                	= extract_date(path)
-sequence_name                       	= extract_sequence_name(path)
+	#extract metadata
+	(sequence_number, repetition_number)	= extract_sequence_repetition_numbers(path)
+	date                                	= extract_date(path)
+	sequence_name                       	= extract_sequence_name(path)
 
-try:
-	#extract data
-	photon_arrival_times = run.get_result_array(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
+	try:
+		#extract data
+		photon_arrival_times = run.get_result_array(group='extract_photon_arrival_times',name='processed_arrivals_ch_1')
 
-	#plot data
-	plt.hist(
-		photon_arrival_times,
-		bins=np.arange(photon_arrival_times[0], photon_arrival_times[-1], photon_arrival_times[-1]/200),
-		align='mid'
-	)
+		#plot data
+		plt.hist(
+			photon_arrival_times,
+			bins=np.arange(photon_arrival_times[0], photon_arrival_times[-1], photon_arrival_times[-1]/200),
+			align='mid'
+		)
 
-	#decorate plot
-	plt.title(f"({date}) #{sequence_number}_r{repetition_number}\n{sequence_name}")
-	plt.ylabel(f"Photon Counts, ({photon_arrival_times[-1]/200  *1000:.3g}ms Bin)")
-	plt.xlabel("Time (s)")
-except:
-	print("No Photon Data to Plot.")
+		#decorate plot
+		plt.title(f"({date}) #{sequence_number}_r{repetition_number}\n{sequence_name}")
+		plt.ylabel(f"Photon Counts, ({photon_arrival_times[-1]/200  *1000:.3g}ms Bin)")
+		plt.xlabel("Time (s)")
+	except:
+		print("No Photon Data to Plot.")
