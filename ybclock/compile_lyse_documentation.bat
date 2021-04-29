d6738aa0f7bf3043c10983d9f1c54b8eed9d838a
Ybclock Operator <vvybclock@gmail.com>
2021-04-29 11:13:50 -0400

    Update User to use %USERPROFILE%

--- ybclock/compile_lyse_documentation.bat
+++ ybclock/compile_lyse_documentation.bat
@@ -1,4 +1,4 @@
-%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\Users\simon\anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\simon\anaconda3' ; conda activate ybclock; cd 'C:\Users\simon\labscript-suite\userlib\labscriptlib\ybclock'; pdoc3 --html lyse --force --skip-errors"
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& '%USERPROFILE%\anaconda3\shell\condabin\conda-hook.ps1' ; conda activate '%USERPROFILE%\anaconda3' ; conda activate ybclock; cd '%USERPROFILE%\labscript-suite\userlib\labscriptlib\ybclock'; pdoc3 --html lyse --force --skip-errors"
 
 
 
