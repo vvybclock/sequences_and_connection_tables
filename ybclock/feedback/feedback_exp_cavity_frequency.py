3ae434cafebd4e9ec49d63f37172c8c69d724ecd
Enrique Mendez <eqm@mit.edu>
2021-05-03 16:26:55 -0400

    Add hdf5 repacking.

--- ybclock/feedback/feedback_exp_cavity_frequency.py
+++ ybclock/feedback/feedback_exp_cavity_frequency.py
@@ -10,39 +10,62 @@
 #make sure the .ini config file has the correct address for the Runmanager!!!
 import runmanager.remote as remote
 from lyse import *
-
+import os
 
 MHz = 1e6
 
-#read cavity frequency
-run = Run(path)
-actual_cavity_frequency = run.get_result('empty_cavity_helper','exp_cavity_frequency')
+#variables for repacking the globals file
+userProfile      	= "C:\\Users\\YbMinutes"
+globals_file     	= os.path.join(userProfile,"labscript-suite\\userlib\\labscriptlib\\ybclock\\globals.h5")
+hdf5repack_file  	= os.path.join(userProfile,r"labscript-suite\userlib\labscriptlib\ybclock\feedback\h5repack.exe")
+temp_globals_file	= os.path.join(userProfile,r"labscript-suite\userlib\labscriptlib\ybclock\temp_globals.h5")
+
+try:
+	#read cavity frequency
+	print("Reading Cavity Frequency...")
+	run = Run(path)
+	actual_cavity_frequency = run.get_result('empty_cavity_helper','exp_cavity_frequency')
 
 
-#get PID variables
-retrieved_globals = remote.get_globals()
+	print("Performing Feedback & Getting Globals...")
+	#get PID variables
+	retrieved_globals = remote.get_globals()
 
-G         	= retrieved_globals['cavity_frequency_G']
-I         	= retrieved_globals['cavity_frequency_I']
-P         	= retrieved_globals['cavity_frequency_P']
-integrator	= retrieved_globals['cavity_frequency_integrator']
+	G         	= retrieved_globals['cavity_frequency_G']
+	I         	= retrieved_globals['cavity_frequency_I']
+	P         	= retrieved_globals['cavity_frequency_P']
+	integrator	= retrieved_globals['cavity_frequency_integrator']
 
-#read desired setpoint
-setpoint	= retrieved_globals['exp_cavity_set_frequency']
+	#read desired setpoint
+	setpoint	= retrieved_globals['exp_cavity_set_frequency']
 
-#perform PID
-error = actual_cavity_frequency - setpoint
+	#perform PID
+	error = actual_cavity_frequency - setpoint
 
-output = -G*(P*error)
+	output = -G*(P*error)
 
-#set cavity frequency 
-retrieved_globals['bridging_frequency_759'] -= output
+	#set cavity frequency 
+	retrieved_globals['bridging_frequency_759'] -= output
 
-#update the global variables
-remote.set_globals(retrieved_globals)
+	#update the global variables
+	print("Setting Global Variables...")
+	remote.set_globals(retrieved_globals)
+
+	#engage the next shot.
+	print("Engaging Next Shot...", end='')
+	remote.engage()
+	print("Done!")
+
+	#repack the globals
+	print("Repacking Globals...",end="")
+	os.rename(globals_file, temp_globals_file)
+	os.system(f'{hdf5repack_file} {temp_globals_file} {globals_file}')
+	os.remove(temp_globals_file)
+	print("Done!")
 
 
+	print("Succesfully Performed Feedback.")
+except Exception as e:
+	print(f"Failed Feedback: {e}")
+	pass
 
-
-#engage the next shot.
-remote.engage()
