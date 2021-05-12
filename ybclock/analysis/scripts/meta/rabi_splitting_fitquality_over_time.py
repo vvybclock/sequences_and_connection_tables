2c696c03d6053f5c44837ecba7c016e847e0a599
Edwin Pedrozo <epedrozo@mit.edu>
2021-05-12 10:26:10 -0400

    Add try,except clauses.

--- ybclock/analysis/scripts/meta/rabi_splitting_fitquality_over_time.py
+++ ybclock/analysis/scripts/meta/rabi_splitting_fitquality_over_time.py
@@ -17,26 +17,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-	#exposes all the variables available in the lyse window
-	dataframe = data()
+	try:
+		#exposes all the variables available in the lyse window
+		dataframe = data()
 
 
-	runtimes = list(dataframe['run time'])
-	paths = list(dataframe['filepath'])
-	fit_quality = list(dataframe['atoms_in_cavity_helper','cavity_scan_fit_chi2'])
-	#this is what we call the run number. we'll use it to change the color, so we can tell when we changed the sequence.
-	sequence_index = list(dataframe['sequence_index'])
+		runtimes = list(dataframe['run time'])
+		paths = list(dataframe['filepath'])
+		fit_quality = list(dataframe['atoms_in_cavity_helper','cavity_scan_fit_chi2'])
+		#this is what we call the run number. we'll use it to change the color, so we can tell when we changed the sequence.
+		sequence_index = list(dataframe['sequence_index'])
 
 
-	#make some colors.
-	cerulean = (4/256,146/256,194/256)
-	crimson = (185/256, 14/256, 10/256)
-	colors = [cerulean if seq%2 == 0 else crimson for seq in sequence_index]
+		#make some colors.
+		cerulean = (4/256,146/256,194/256)
+		crimson = (185/256, 14/256, 10/256)
+		colors = [cerulean if seq%2 == 0 else crimson for seq in sequence_index]
 
 
-	#s - size
-	plt.scatter(runtimes, fit_quality, s=20, c = colors)
+		#s - size
+		plt.scatter(runtimes, fit_quality, s=20, c = colors)
 
-	plt.title("Rabi Splitting Fit Quality")
-	plt.ylabel("chi squared")
-	plt.xlabel("Time")
+		plt.title("Rabi Splitting Fit Quality")
+		plt.ylabel("chi squared")
+		plt.xlabel("Time")
+	except Exception as e:
+		print(f"Rabi Splitting: {e}")
