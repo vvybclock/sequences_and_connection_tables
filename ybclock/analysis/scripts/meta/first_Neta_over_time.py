0b7481a5ac269e2344511aab150e5a958fc355dd
Simone <colombos@mit.edu>
2021-05-14 11:23:21 -0400

    Meta-analysis of first Neta. Select only good fits.

--- ybclock/analysis/scripts/meta/first_Neta_over_time.py
+++ ybclock/analysis/scripts/meta/first_Neta_over_time.py
@@ -1,13 +1,15 @@
 ''' This metanalysis will plot and analyse the initially loaded Neta versus the time. It
-will be used to monitor how fitting Rabi splitting works over the time and/or
+will be used: 
+i) to monitor how fitting Rabi splitting works over the time and/or
 as a function of experimental parameters
+ii) to test the loading efficiency, the output can be used to optimize loading
 
  # To Do 
- 	[] make atoms_in_cavity_helper.py write the Neta value per scan number of fit in lyse parameters
+ 	[x] make atoms_in_cavity_helper.py write the Neta 1 value per scan number of fit in lyse parameters
  	[] read lyse parameters here
  	[] drop Neta based on bad chi^2 
- 	[] statistics (hist plot, for example) of chi^2 results
- 	[] statistics versus some interesting parameter, like photons detected for example
+ 	[] multiple scan number and different color for different scan number in the sequence
+ 	[] statistics (hist plot, for example) of Neta results
 
 '''
 
@@ -24,33 +26,45 @@ if __name__ == '__main__':
 
 		runtimes = list(dataframe['run time'])
 		paths = list(dataframe['filepath'])
-		fit_quality = list(dataframe['atoms_in_cavity_helper','cavity_scan_fit_chi2'])
+		fit_1_quality = list(dataframe['atoms_in_cavity_helper','chi_square_1'])
+		Neta_1_list = list(dataframe['atoms_in_cavity_helper','Neta_1'])
 		#this is what we call the run number. we'll use it to change the color, so we can tell when we changed the sequence.
 		sequence_index = list(dataframe['sequence_index'])
-
 
 		#make some colors.
 		cerulean = (4/256,146/256,194/256)
 		crimson = (185/256, 14/256, 10/256)
 		colors = [cerulean if seq%2 == 0 else crimson for seq in sequence_index]
+		blue = [(0,0,1)]
+		red = [(1,0,0)]
 
 		#Calculate and print Good Fit Ratio
-		good_chi_for_fit = [0.5,10]
+		good_chi_for_fit = [0.4,10]
 
 		is_good_fit = []
-		for set_of_fits in fit_quality:
-			for fit in set_of_fits:
-				is_good_fit.append(
-						(fit > good_chi_for_fit[0]) and (fit < good_chi_for_fit[1])
-					)
+		for fit in fit_1_quality:
+			is_good_fit.append(
+					(fit > good_chi_for_fit[0]) and (fit < good_chi_for_fit[1])
+				)
+		
+		# create a list of lists with data we may want to plot, with `is_good_fit`. as the first element of each line.
+		data_for_fit = np.transpose([fit_1_quality, sequence_index, runtimes, Neta_1_list]) # 
+		# select good runs
+		data_sel_for_fit = []
+		for data_set in data_for_fit:
+				fit = data_set[0]
+				if (fit > good_chi_for_fit[0]) and (fit < good_chi_for_fit[1]):
+					data_sel_for_fit.append(data_set)
+		data_sel_for_fit = np.transpose(data_sel_for_fit)
 
-		good_fit_ratio = sum(is_good_fit)/len(is_good_fit)
 
 		#s - size
-		plt.scatter(runtimes, fit_quality, s=20, c = colors)
+		plt.scatter(runtimes, Neta_1_list, s=20, c = red)
 
-		plt.title("Rabi Splitting Fit Quality\n" + f"Good Fit Ratio: {100*good_fit_ratio:.1f}%")
-		plt.ylabel("chi squared")
+		plt.scatter(data_sel_for_fit[-2], data_sel_for_fit[-1], s=40, c = blue)
+
+		plt.title("Fitted Neta" )
+		plt.ylabel("Neta")
 		plt.xlabel("Time")
 
 
