0d3cd15dcd1d976ef628c6de568fd06a1e8434b0
Simone <colombos@mit.edu>
2021-05-18 12:06:36 -0400

    Metanalysis of multiple atom_cavity_scan.

--- ybclock/analysis/scripts/meta/Neta_over_time.py
+++ ybclock/analysis/scripts/meta/Neta_over_time.py
@@ -1,12 +1,13 @@
-''' This metanalysis will plot and analyse the initially loaded Neta versus the time. It
-will be used to monitor how fitting Rabi splitting works over the time and/or
-as a function of experimental parameters
+''' This metanalysis will plot and analyse multiple Neta measured in sequences. It can be used for various applications as, for example:
+i) Monitor or study pump efficiency
+ii) Cooling efficiency
+iii) basis for any Sz measurement (2, 4, 8 -measurements...)
 
  # To Do 
  	[x] make atoms_in_cavity_helper.py write the Neta 1 value per scan number of fit in lyse parameters
- 	[] read lyse parameters here
- 	[] drop Neta based on bad chi^2 
- 	[] multiple scan number and different color for different scan number in the sequence
+ 	[x] read lyse parameters here
+ 	[] drop Neta based on bad chi^2
+ 	[x] Multiple Neta fit versus time
  	[] statistics (hist plot, for example) of Neta results
 
 '''
@@ -15,56 +16,49 @@ from lyse import *
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 if __name__ == '__main__':
 	try:
 		#exposes all the variables available in the lyse window
 		dataframe = data()
 
-
 		runtimes = list(dataframe['run time'])
 		paths = list(dataframe['filepath'])
-		fit_quality = list(dataframe['empty_cavity_helper','cavity_scan_fit_chi2'])
-		Neta_1 = list(dataframe['atoms_in_cavity_helper','Neta_1'])
-		#this is what we call the run number. we'll use it to change the color, so we can tell when we changed the sequence.
-		sequence_index = list(dataframe['sequence_index'])
 
+		# set the maximal number of scan we can detect in one sequence
+		n_max = 101
 
-		#make some colors.
-		cerulean = (4/256,146/256,194/256)
-		crimson = (185/256, 14/256, 10/256)
-		colors = [cerulean if seq%2 == 0 else crimson for seq in sequence_index]
+		# create a new Pandas (a new dataframe) that allows for rapid plotting of multiple traces. Color coding and legend are already built-in!
+		Neta_dic = {}
+		chi_square_dic = {}
+		df=pd.DataFrame()
+		j=0
+		while j < n_max:
+			try:
+				newname = "Neta_"+str(j+1)
+				chi_newname = "chi_square_"+str(j+1)
+				Neta_dic[newname] = list(dataframe['atoms_in_cavity_helper',newname])
+				chi_square_dic[newname] = list(dataframe['atoms_in_cavity_helper',chi_newname])
+				dfl = pd.DataFrame(np.transpose([list(dataframe['atoms_in_cavity_helper',newname])]),index = runtimes,columns = [newname])
+				df = df.append(dfl)
+				#df=pd.concat([pd.DataFrame([i], columns=[newname]) for i in Neta_dic[newname]],
+					#ignore_index=True)
+				j+=1
+			except Exception as e:
+				if j == 0:
+					print("Failed importing data. Error : ",e)
+				else:
+					print(j ," Rabi splitting scan found.")
+				break
 
-		#Calculate and print Good Fit Ratio
-		good_chi_for_fit = [0.4,25]
-
-		is_good_fit = []
-		for set_of_fits in fit_quality:
-			is_good_set_fit = []
-			for fit in set_of_fits:
-				is_good_set_fit.append(
-						(fit > good_chi_for_fit[0]) and (fit < good_chi_for_fit[1])
-					)
-			is_good_fit.append(is_good_set_fit)
-
-
-		# select good runs
-		# Neta_sel = []
-		# runtimes_sel = []
-		# for i in range(len(is_good_fit)):
-		# 	for j in range(len(is_good_fit[i])):
-		# 		if is_good_fit[i][j]:
-		# 			Neta_sel.append(Neta_list[i][j])
-		# 			runtimes_sel.append(runtimes[i])
-
-		blue = [(0,0,1)]
-		#s - size
-		plt.scatter(runtimes, Neta_1, s=20, c = blue)
-
+		plt.figure(); df.plot();
 		plt.title("Fitted Neta" )
 		plt.ylabel("Neta")
 		plt.xlabel("Time")
 
+		#Calculate and print Good Fit Ratio
+		good_chi_for_fit = [0.4,40]
 
 	except Exception as e:
-		print(f"Rabi Splitting: {e}")
+		print("get_keys Error: ", e)
