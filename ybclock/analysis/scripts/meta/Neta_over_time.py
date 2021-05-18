808043772adf98cd3f34013072e35b91dcaa748b
Simone <colombos@mit.edu>
2021-05-18 12:21:35 -0400

    Postselection based on Chi_square in multiple atom_cavity_scan metanalysis.

--- ybclock/analysis/scripts/meta/Neta_over_time.py
+++ ybclock/analysis/scripts/meta/Neta_over_time.py
@@ -6,7 +6,7 @@ iii) basis for any Sz measurement (2, 4, 8 -measurements...)
  # To Do 
  	[x] make atoms_in_cavity_helper.py write the Neta 1 value per scan number of fit in lyse parameters
  	[x] read lyse parameters here
- 	[] drop Neta based on bad chi^2
+ 	[x] drop Neta based on bad chi^2
  	[x] Multiple Neta fit versus time
  	[] statistics (hist plot, for example) of Neta results
 
@@ -29,18 +29,26 @@ if __name__ == '__main__':
 		# set the maximal number of scan we can detect in one sequence
 		n_max = 101
 
+		good_chi_for_fit = [0.4,40]
 		# create a new Pandas (a new dataframe) that allows for rapid plotting of multiple traces. Color coding and legend are already built-in!
 		Neta_dic = {}
 		chi_square_dic = {}
+		runtimes_dic = {};
 		df=pd.DataFrame()
 		j=0
 		while j < n_max:
 			try:
 				newname = "Neta_"+str(j+1)
 				chi_newname = "chi_square_"+str(j+1)
-				Neta_dic[newname] = list(dataframe['atoms_in_cavity_helper',newname])
+				Neta_loc= list(dataframe['atoms_in_cavity_helper',newname])
 				chi_square_dic[newname] = list(dataframe['atoms_in_cavity_helper',chi_newname])
-				dfl = pd.DataFrame(np.transpose([list(dataframe['atoms_in_cavity_helper',newname])]),index = runtimes,columns = [newname])
+				indices = [i for i, x in enumerate(chi_square_dic[newname]) if x < good_chi_for_fit[1] and x > good_chi_for_fit[0]]
+				Neta_dic[newname] = []
+				runtimes_dic[newname] = []
+				for kk in indices:
+					Neta_dic[newname].append(Neta_loc[kk])
+					runtimes_dic[newname].append(runtimes[kk])
+				dfl = pd.DataFrame(Neta_dic[newname],index = runtimes_dic[newname],columns = [newname])
 				df = df.append(dfl)
 				#df=pd.concat([pd.DataFrame([i], columns=[newname]) for i in Neta_dic[newname]],
 					#ignore_index=True)
@@ -58,7 +66,7 @@ if __name__ == '__main__':
 		plt.xlabel("Time")
 
 		#Calculate and print Good Fit Ratio
-		good_chi_for_fit = [0.4,40]
+		
 
 	except Exception as e:
 		print("get_keys Error: ", e)
