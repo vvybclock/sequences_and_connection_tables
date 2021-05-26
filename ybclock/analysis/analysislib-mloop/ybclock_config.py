2ef657f7491dc1e7d1971d004a7576c3fafd13df
Enrique Mendez <eqm@mit.edu>
2021-05-26 12:31:06 -0400

    Create opt_loading_config test

--- ybclock/analysis/analysislib-mloop/ybclock_config.py
+++ ybclock/analysis/analysislib-mloop/ybclock_config.py
@@ -1,5 +1,19 @@
+'''
+	A ybclock custom script for defining the settings for MLOOP.
 
+	For different optimization sequences change `get()`.
+
+'''
+from opt_loading import *
 
 
 def get():
-	return params
+	'''
+		
+		Returns the `dict()` that contains all the settings for the MLOOP
+		controller.
+
+		For a comprehensive list of settings possible to set, see [the MLOOP API](https://m-loop.readthedocs.io/en/stable/api/controllers.html)
+
+	'''
+	return opt_loading_config()
