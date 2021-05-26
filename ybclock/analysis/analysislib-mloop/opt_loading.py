bb62bfee40d9d8ea96b847d538adf242922dcc63
Enrique Mendez <eqm@mit.edu>
2021-05-26 15:58:00 -0400

    Add more params.

--- ybclock/analysis/analysislib-mloop/opt_loading.py
+++ ybclock/analysis/analysislib-mloop/opt_loading.py
@@ -9,15 +9,19 @@ def opt_loading_config():
 	#ANALYSIS
 	s['cost_key']	= ['generate_cost', 'Neta']
 	s['maximize']	= True
-	s['ignore_bad'] = False
+	s['ignore_bad'] = True
 
 	#MLOOP
 	s['mloop_params']	=  {
-	                 	'blue_mot_duration': {'min': 0, 'max': 3.0, 'start': 0.1},
+	                 	'blue_mot_duration': {'min': 0.1, 'max': 3.0, 'start': 0.2},
 	}
+
 	s['num_training_runs']	= 50
 	s['trust_region']     	= 0.5
 	s['cost_has_noise']   	= True
 	s['controller_type']  	= 'gaussian_process'
+	s['visualisations']   	= True
 
 	return s
+
+
