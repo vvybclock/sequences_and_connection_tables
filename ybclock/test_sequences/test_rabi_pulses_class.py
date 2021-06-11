3c1b2ba4d7393b38e2f73fc19475d010ca2d49ee
Enrique Mendez <eqm@mit.edu>
2021-06-11 11:20:33 -0400

    Tested theoretical rabi pulses to be accurate.

--- ybclock/test_sequences/test_rabi_pulses_class.py
+++ ybclock/test_sequences/test_rabi_pulses_class.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
 	print("Test Spinor Class...")
 
-	atom = Spinor(w_larmor = 10e3)
+	atom = Spinor(w_larmor = 10)
 
 	atom.prepare_atom_unitary(t=0)
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 	print(f"Atom Unitary\n {atom.unitary}")
 	print(f"Evolution Unitary\n {atom.U_V([200000000*pi])}")
 	print(f"tlast: {atom.t_last}")
-	print(f"Net Unitary\n {atom.rabi_pulse(t=0.1, duration=0)}")
+	print(f"Net Unitary\n {atom.rabi_pulse(t=0.1, duration=10, Omega=[pi],w_rf=10)}")
 	start()
 	
 	stop(1)
