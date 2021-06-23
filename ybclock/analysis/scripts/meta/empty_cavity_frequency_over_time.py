1a02d7762394bfbbfd7b5d167fa6f3678966c403
Enrique Mendez <eqm@mit.edu>
2021-06-23 15:47:46 -0400

    Fix error that breaks loop.

--- ybclock/analysis/scripts/meta/empty_cavity_frequency_over_time.py
+++ ybclock/analysis/scripts/meta/empty_cavity_frequency_over_time.py
@@ -13,26 +13,29 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-	#exposes all the variables available in the lyse window
-	dataframe = data()
+	try:
+		#exposes all the variables available in the lyse window
+		dataframe = data()
 
 
-	runtimes = list(dataframe['run time'])
-	paths = list(dataframe['filepath'])
-	frequencies = list(dataframe['empty_cavity_helper','fcavity_1'])
-	#this is what we call the run number. we'll use it to change the color, so we can tell when we changed the sequence.
-	sequence_index = list(dataframe['sequence_index'])
+		runtimes = list(dataframe['run time'])
+		paths = list(dataframe['filepath'])
+		frequencies = list(dataframe['empty_cavity_helper','fcavity_1'])
+		#this is what we call the run number. we'll use it to change the color, so we can tell when we changed the sequence.
+		sequence_index = list(dataframe['sequence_index'])
 
 
-	#make some colors.
-	cerulean = (4/256,146/256,194/256)
-	crimson = (185/256, 14/256, 10/256)
-	colors = [cerulean if seq%2 == 0 else crimson for seq in sequence_index]
+		#make some colors.
+		cerulean = (4/256,146/256,194/256)
+		crimson = (185/256, 14/256, 10/256)
+		colors = [cerulean if seq%2 == 0 else crimson for seq in sequence_index]
 
 
-	#s - size
-	plt.scatter(runtimes, frequencies, s=20, c = colors)
+		#s - size
+		plt.scatter(runtimes, frequencies, s=20, c = colors)
 
-	plt.title("Empty Cavity Frequency")
-	plt.ylabel("Frequency (MHz)")
-	plt.xlabel("Time")
+		plt.title("Empty Cavity Frequency")
+		plt.ylabel("Frequency (MHz)")
+		plt.xlabel("Time")
+	except Exception as e:
+		print(e)
